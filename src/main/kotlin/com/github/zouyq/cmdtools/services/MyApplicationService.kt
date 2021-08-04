bd9ea68f88f2d0cd8154e04f90b4da6331795483
GitHub Action <action@github.com>
2021-08-04 09:44:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/zouyq/cmdtools/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zouyq/cmdtools/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zouyq.cmdtools.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zouyq.cmdtools.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/zouyq/cmdtools/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zouyq/cmdtools/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zouyq.cmdtools.services
 
+import com.github.zouyq.cmdtools.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
